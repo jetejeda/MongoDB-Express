f0cfb96ccac2ff9444b86869ba999ac622d3c35c
jetejeda <eduardotejedaleon@gmail.com>
2022-07-25 16:38:59 -0600

    Create and read methods implemented

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -5,12 +5,23 @@ const Task = require('../model/Task')();
 router.get('/', (req, res)=>{
 
     Task.find({}, (err, tasks)=>{
-        if(err) console.log(err);
+        if(err) throw err;
         res.render('index',{
-            task: tasks
+            tasks: tasks,
+            title: 'TITLE'
         })
     });
 
 });
 
+//Post for tasks creation:
+router.post('/add', (req, res)=>{
+    let body = req.body;
+    body.Status = false;
+
+    Task.create(body, (err, task)=>{
+        if(err) throw err
+        res.redirect('/')
+    })
+});
 module.exports = router;
